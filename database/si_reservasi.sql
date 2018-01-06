cf6b9a0fd3a2cece6a886b7f6d8510a72bb25690
chandradhrmawan <chandradarmawan17@gmail.com>
2018-01-06 15:34:22 +0700

    database new

--- database/si_reservasi.sql
+++ database/si_reservasi.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 18 Des 2017 pada 16.45
+-- Generation Time: 06 Jan 2018 pada 09.33
 -- Versi Server: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -49,7 +49,10 @@ INSERT INTO `detail_sewa` (`id_detail`, `id_sewa`, `id_barang`, `jumlah`, `statu
 (2, 'SW006151217', '3', 4, '1'),
 (3, 'SW006151217', '4', 1, '1'),
 (4, 'SW007101217', '4', 8, '0'),
-(5, 'SW007101217', '3', 2, '0');
+(5, 'SW007101217', '3', 2, '0'),
+(1, 'SW008030118', '1', 4, '1'),
+(2, 'SW008030118', '3', 2, '1'),
+(3, 'SW009030118', '3', 2, '1');
 
 -- --------------------------------------------------------
 
@@ -29174,9 +29177,9 @@ CREATE TABLE IF NOT EXISTS `m_barang` (
 --
 
 INSERT INTO `m_barang` (`id_barang`, `nama_barang`, `tgl_ditambakan`, `id_merk`, `id_kategori`, `id_warna`, `id_ukuran`, `deskripsi`, `harga_sewa`, `harga_beli`, `stok`, `status`, `foto`) VALUES
-(1, 'Tenda Dome Kap 4', '2017-12-01 15:46:05', 1, 1, 1, 1, 'Tenda Dome Kapasitas 4 Orang', 25000, '1500000', 16, 1, 'B00007.jpg'),
+(1, 'Tenda Dome Kap 4', '2017-12-01 15:46:05', 1, 1, 1, 1, 'Tenda Dome Kapasitas 4 Orang', 25000, '1500000', 12, 1, 'B00007.jpg'),
 (2, 'Jacket Eiger', '2017-12-01 12:55:27', 1, 3, 1, 1, 'Deskripsi Barang', 10000, '1500000', 10, 1, 'B00011.jpg'),
-(3, 'JAKET', '2017-12-01 15:06:35', 1, 1, 1, 1, 'Deskripsi Barang', 5000, '10000', 8, 1, 'B00008.jpg'),
+(3, 'JAKET', '2017-12-01 15:06:35', 1, 1, 1, 1, 'Deskripsi Barang', 5000, '10000', 6, 1, 'B00008.jpg'),
 (4, 'KACAMATAN', '2017-12-01 15:06:45', 1, 1, 1, 1, 'Deskripsi Barang', 5000, '10000', 8, 1, 'B00024.jpg'),
 (5, '5', '2017-12-01 15:07:03', 1, 1, 1, 1, 'Deskripsi Barang', 5, '5', 10, 1, 'B00026.jpg'),
 (6, '6', '2017-12-01 15:07:16', 1, 1, 1, 1, 'Deskripsi Barang', 6, '6', 10, 1, 'B00028.jpg'),
@@ -29267,15 +29270,16 @@ CREATE TABLE IF NOT EXISTS `m_user` (
   `kode_pos` varchar(255) DEFAULT NULL,
   `alamat_lengkap` varchar(255) DEFAULT NULL,
   `status` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data untuk tabel `m_user`
 --
 
 INSERT INTO `m_user` (`id_user`, `tgl_daftar`, `nama_user`, `username`, `email`, `password`, `id_provinsi`, `id_kota`, `id_kecamatan`, `kode_pos`, `alamat_lengkap`, `status`) VALUES
-(1, '2017-11-27 16:20:24', 'rifaldi rahman', 'user', 'rifaldi@gmail.com', 'user', '13', '1301', '1301013', '14320', 'Jakarta Utara ', '1'),
-(2, '2017-11-27 16:20:24', '123', 'asdasd', '123@asd.123', '123', '12', '1219', '1219040', '123', '123. ', '1');
+(1, '2018-01-04 00:20:07', 'rony', 'user', 'rifaldi@gmail.com', 'user', '13', '1301', '1301013', '14320', 'Jakarta Utara ', '1'),
+(2, '2017-11-27 16:20:24', '123', 'asdasd', '123@asd.123', '123', '12', '1219', '1219040', '123', '123. ', '1'),
+(3, '2018-01-03 18:23:58', 'rony', 'rony', 'rony@gmail.com', 'rony', '11', '1107', '1107050', '123', ' 123', '1');
 
 -- --------------------------------------------------------
 
@@ -29326,7 +29330,8 @@ INSERT INTO `pembayaran_awal` (`id_pembayaran`, `id_sewa`, `tgl_pembayaran`, `id
 ('DP003021217', 'SW004021217', '2017-12-02 17:47:35', 2, 'BNN', '1232323', '123', 50000, 'B00027.jpg', 'SSS'),
 ('DP004101217', 'SW005101217', '2017-12-10 10:20:33', 1, '123', '123', '123', 1, 'check.png', 'a'),
 ('DP005101217', 'SW007101217', '2017-12-10 10:39:15', 2, '11', '11', '11', 25000, '24.jpg', '123'),
-('DP006181217', 'SW006151217', '2017-12-18 10:42:10', 2, 'aa', '123', '2323', 12500, '15.jpg', 'aaaa');
+('DP006181217', 'SW006151217', '2017-12-18 10:42:10', 2, 'aa', '123', '2323', 12500, '15.jpg', 'aaaa'),
+('DP007030118', 'SW008030118', '2018-01-03 18:15:16', 2, '123', '123', '123', 55000, '7.png', 'asdasd');
 
 -- --------------------------------------------------------
 
@@ -29351,7 +29356,8 @@ INSERT INTO `pengembalian` (`id_pengembalian`, `tgl_pengembalian`, `id_user`, `i
 ('PN003021217', '2017-12-02 17:48:23', '1', 'SW004021217'),
 ('PN004101217', '2017-12-10 10:19:23', '1', 'SW001011217'),
 ('PN005151217', '2017-12-15 10:33:23', '1', 'SW005101217'),
-('PN006181217', '2017-12-18 10:41:47', '1', 'SW007101217');
+('PN006181217', '2017-12-18 10:41:47', '1', 'SW007101217'),
+('PN007030118', '2018-01-03 18:17:07', '1', 'SW008030118');
 
 -- --------------------------------------------------------
 
@@ -29551,7 +29557,9 @@ INSERT INTO `sewa` (`id_sewa`, `id_user`, `tgl_sewa`, `tgl_selesai`, `tgl_expire
 ('SW004021217', 1, '2017-12-03', '2017-12-09', '2017-12-04', 3, 3, 100000, 50000),
 ('SW005101217', 1, '2017-12-11', '2017-12-13', '2017-12-12', 3, 3, 2, 1),
 ('SW006151217', 1, '2017-12-11', '2017-12-13', '0000-00-00', 2, 4, 25000, 12500),
-('SW007101217', 1, '2017-12-11', '2017-12-16', '2017-12-12', 3, 3, 50000, 25000);
+('SW007101217', 1, '2017-12-11', '2017-12-16', '2017-12-12', 3, 3, 50000, 25000),
+('SW008030118', 1, '2018-01-05', '2018-01-07', '2018-01-06', 3, 3, 110000, 55000),
+('SW009030118', 3, '0000-00-00', '0000-00-00', '0000-00-00', 0, 0, 10000, 0);
 
 -- --------------------------------------------------------
 
@@ -29565,7 +29573,7 @@ CREATE TABLE IF NOT EXISTS `tmp_detail_sewa` (
   `id_barang` varchar(255) DEFAULT NULL,
   `jumlah` int(11) DEFAULT NULL,
   `status_pinjam` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Indexes for dumped tables
@@ -29687,7 +29695,7 @@ ALTER TABLE `m_ukuran`
 -- AUTO_INCREMENT for table `m_user`
 --
 ALTER TABLE `m_user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `m_warna`
 --
@@ -29697,7 +29705,7 @@ ALTER TABLE `m_warna`
 -- AUTO_INCREMENT for table `tmp_detail_sewa`
 --
 ALTER TABLE `tmp_detail_sewa`
-  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+  MODIFY `id_detail` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
